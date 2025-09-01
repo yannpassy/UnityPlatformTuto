84ed4e133160644fbe0f4d8b33ba6a5a2a445d62
yannpassy <yannpassy1@gmail.com>
2025-09-01 12:23:41 +0200

    Update SpikeHead.cs
    calculate direction

--- Assets/Scripts/Ennemies/SpikeHead.cs
+++ Assets/Scripts/Ennemies/SpikeHead.cs
@@ -10,11 +10,35 @@ public class SpikeHead : EnnemyDamage
     private float checkTimer;
     private Vector3 destination;
     private bool attacking;
+    private Vector3[] directions = new Vector3[4];
 
     private void Update()
     {
         // move spikehead to its final destination only if attacking
         if (attacking)
             transform.Translate(destination * Time.deltaTime * speed);
+        else
+        {
+            checkTimer += Time.deltaTime;
+            if (checkTimer > checkDelay)
+            {
+                CheckForPlayer();
+            }
+        }
+    }
+
+    private void CheckForPlayer()
+    {
+        CalculateDirections();
+        // check if spikeHead sees directions 
+        
+    }
+
+    private void CalculateDirections()
+    {
+        directions[0] = transform.right * range; // right direction
+        directions[1] = - transform.right * range; // left direction
+        directions[2] = transform.up * range; // up direction
+        directions[3] = - transform.up * range; // down direction
     }
 }
