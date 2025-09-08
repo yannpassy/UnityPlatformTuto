deb05e3a5f5b3e1a8ffe16449bd12324a1d77114
yannpassy <yannpassy1@gmail.com>
2025-09-08 14:03:43 +0200

    finishing spikehead

--- Assets/Scripts/Ennemies/SpikeHead.cs
+++ Assets/Scripts/Ennemies/SpikeHead.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class SpikeHead : EnnemyDamage
 {
+    [Header ("Spikehead attribut")]
     [SerializeField] private float speed;
     [SerializeField] private float range;
     [SerializeField] private float checkDelay;
+    [SerializeField] private LayerMask playerLayer;
     private float checkTimer;
     private Vector3 destination;
     private bool attacking;
     private Vector3[] directions = new Vector3[4];
 
+
+    private void OnEnable()
+    {
+        Stop();
+    }
     private void Update()
     {
         // move spikehead to its final destination only if attacking
@@ -30,15 +37,43 @@ public class SpikeHead : EnnemyDamage
     private void CheckForPlayer()
     {
         CalculateDirections();
-        // check if spikeHead sees directions 
-        
+
+        // spikeHead sees directions in all direction
+        foreach (Vector3 direction in directions)
+        {
+            Debug.DrawRay(transform.position, direction, Color.red); // for the log with Gizmos
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, range, playerLayer); // crée un laser invisble qui detecte uniquement le joueur via son layer
+
+            // si le joueur est touché par le raycast et spikehead n'est pas en train d'attaquer
+            if (hit.collider != null && !attacking)
+            {
+                attacking = true;
+                destination = direction;
+                checkTimer = 0;
+            }
+        }
+
     }
 
     private void CalculateDirections()
     {
         directions[0] = transform.right * range; // right direction
-        directions[1] = - transform.right * range; // left direction
+        directions[1] = -transform.right * range; // left direction
         directions[2] = transform.up * range; // up direction
-        directions[3] = - transform.up * range; // down direction
+        directions[3] = -transform.up * range; // down direction
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+        //stop Spikehead when it hit something
+        Stop();
+
+    }
+
+    private void Stop()
+    {
+        destination = transform.position; // stop the spikehead by setting the destination by its position, so it doesn't move
+        attacking = false;
     }
 }
