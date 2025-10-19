a0252c223e1ef79494f1154672d5ad165cf4d3e6
yannpassy <yannpassy1@gmail.com>
2025-10-19 20:11:59 +0200

    move melee attack
    10(25:15)

--- Assets/Scripts/Ennemy/EnnemyPatrol.cs
+++ Assets/Scripts/Ennemy/EnnemyPatrol.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class EnnemyPatrol : MonoBehaviour
 {
@@ -10,10 +11,35 @@ public class EnnemyPatrol : MonoBehaviour
 
     [Header("Ennemy")]
     [SerializeField] private Transform ennemy;
+
+    [Header("Movement parameter")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    private void Awake() {
+        initScale = ennemy.localScale;
+    }
+
+    private void Update() {
+        if (movingLeft)
+        {
+            moveInDirection(-1);
+        }
+        else
+        {
+            moveInDirection(1);
+        }
+        
+    }
     private void moveInDirection(int _direction)
     {
         // Make ennemy face direction
+        ennemy.localScale = new Vector3(Mathf.Abs(ennemy.localScale.x) * _direction,
+            ennemy.localScale.y, ennemy.localScale.z);
 
         //Move in that direction
+        ennemy.position = new Vector3(ennemy.position.x + Time.deltaTime * speed * _direction,
+            ennemy.position.y, ennemy.position.z);
     }
 }
